986da58878b1bc1f5067674efdf03c15265e6d73
Paolo Arcagni <paolo.arcagni@gmail.com>
2021-02-11 07:49:53 +0100

    initial commit

--- init-scripts/3.create-kubeconfig.sh
+++ init-scripts/3.create-kubeconfig.sh
@@ -9,7 +9,7 @@ K8S_CLIENT_CERT_DATA=$(cat ~/.kube/config-k8s | grep client-certificate-data | a
 K3S_CLIENT_KEY_DATA=$(cat ~/.kube/config-k3s | grep client-key-data | awk '{print $2}')
 K8S_CLIENT_KEY_DATA=$(cat ~/.kube/config-k8s | grep client-key-data | awk '{print $2}')
 
-sed "s/<PUT HERE YOUR K3S CERTIFICATE AUTHORITY DATA>/$K3S_CERT_AUTH_DATA/" ~/f5-udf-infra-server/ansible/playbooks/files/config > ~/.kube/config
+sed "s/<PUT HERE YOUR K3S CERTIFICATE AUTHORITY DATA>/$K3S_CERT_AUTH_DATA/" ~/azure-infra-server/ansible/playbooks/files/config > ~/.kube/config
 sed -i "s/<PUT HERE YOUR K8S CERTIFICATE AUTHORITY DATA>/$K8S_CERT_AUTH_DATA/" ~/.kube/config
 sed -i "s/<PUT HERE YOUR K3S CLIENT CERTIFICATE DATA>/$K3S_CLIENT_CERT_DATA/" ~/.kube/config
 sed -i "s/<PUT HERE YOUR K8S CLIENT CERTIFICATE DATA>/$K8S_CLIENT_CERT_DATA/" ~/.kube/config
